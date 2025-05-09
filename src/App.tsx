a385348b5da96ed445b86922e44617dadc01e75c
Luis Fernando Farji <fernando.farji@gmail.com>
2025-05-09 14:41:32 -0300

    Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+
+import React, { useReducer } from 'react';
 import './App.css';
 
+/* 1- Create a initialState */
 const initialState = {
   fromLanguage: 'auto',
   toLanguage: 'en',
@@ -10,8 +12,52 @@ const initialState = {
   loading: false
 }
 
+/* 2- Create a context / Reducer */
+function reducer (state, action) {
+
+  const {type, payload } = action
+
+  if (type === 'INTERCHANGE_LANGUAGE') {
+    return{
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    } 
+}
+if (type === 'SET_FROM_LANGUAGE') {
+   return{
+     ...state,
+     fromLanguage: payload
+    }
+  }
+if (type === 'SET_TO_LANGUAGE') {
+    return{
+      ...state,
+      toLanguage: payload
+    }
+  }
+if (type === 'SET_FROM_TEXT') {
+    return{
+      ...state,
+      loading: true,
+      fromText: payload,
+      result: ''
+    }
+  }
+if (type === 'SET_RESULT') {
+    return{
+      ...state,
+      loading: false,
+      result: payload
+    }
+  }
+
+return state
+
+}
 function App() {
-  
+  /* 3- Usar el Hook useReducer */
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
       <h1>Google Translate</h1>
@@ -20,3 +66,4 @@ function App() {
 }
 
 export default App
+ 
